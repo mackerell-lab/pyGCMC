8790092611130b6813b986026835c0ed338aac0e
Xenopelt <zhaomingtian@gmail.com>
2025-01-31 06:08:44 -0500

    good

--- src/bindings/io_bindings.cpp
+++ src/bindings/io_bindings.cpp
@@ -3,12 +3,8 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "io/pdbParser.hpp"
-#include "model/structure.hpp"
 #include "io/psfParser.hpp"
 #include "io/topParser.hpp"
-#include "model/atom.hpp"
-#include "model/residue.hpp"
-#include "model/forcefield.hpp"
 #include "io/prmParser.hpp"
 
 namespace py = pybind11;
@@ -20,56 +16,6 @@ void init_io(py::module& m) {
     // Create io submodule
     auto io = m.def_submodule("io", "Input/Output operations");
     
-    // Bind Structure::SecondaryStructure
-    auto secondary_structure = py::class_<model::Structure::SecondaryStructure>(m, "SecondaryStructure")
-        .def(py::init<>())
-        .def_readwrite("id", &model::Structure::SecondaryStructure::id)
-        .def_readwrite("initResName", &model::Structure::SecondaryStructure::initResName)
-        .def_readwrite("initChainId", &model::Structure::SecondaryStructure::initChainId)
-        .def_readwrite("initSeqNum", &model::Structure::SecondaryStructure::initSeqNum)
-        .def_readwrite("initICode", &model::Structure::SecondaryStructure::initICode)
-        .def_readwrite("endResName", &model::Structure::SecondaryStructure::endResName)
-        .def_readwrite("endChainId", &model::Structure::SecondaryStructure::endChainId)
-        .def_readwrite("endSeqNum", &model::Structure::SecondaryStructure::endSeqNum)
-        .def_readwrite("endICode", &model::Structure::SecondaryStructure::endICode)
-        .def_readwrite("helixClass", &model::Structure::SecondaryStructure::structureClass);
-
-    // Bind Structure::TerminalInfo
-    auto terminal_info = py::class_<model::Structure::TerminalInfo>(m, "TerminalInfo")
-        .def(py::init<>())
-        .def_readwrite("chainId", &model::Structure::TerminalInfo::chainId)
-        .def_readwrite("resSeq", &model::Structure::TerminalInfo::resSeq)
-        .def_readwrite("iCode", &model::Structure::TerminalInfo::iCode)
-        .def_readwrite("resName", &model::Structure::TerminalInfo::resName);
-
-    // Bind Structure to main module
-    auto structure = py::class_<model::Structure>(m, "Structure")
-        .def(py::init<>())
-        // 直接暴露内部成员作为属性
-        .def_property_readonly("atoms", [](const model::Structure& s) { return s.get_atoms(); },
-            "List of atoms in the structure")
-        .def_property_readonly("residues", [](const model::Structure& s) { return s.get_residues(); },
-            "List of residues in the structure")
-        .def_property_readonly("terminals", [](const model::Structure& s) { return s.get_terminals(); },
-            "List of terminal records")
-        .def_property_readonly("helices", [](const model::Structure& s) { return s.get_helices(); },
-            "Map of chain IDs to helix information")
-        .def_property_readonly("sheets", [](const model::Structure& s) { return s.get_sheets(); },
-            "Map of chain IDs to sheet information")
-        .def_property_readonly("ssbonds", [](const model::Structure& s) { return s.get_ssbonds(); },
-            "List of disulfide bonds")
-        .def_property_readonly("box_dimensions", [](const model::Structure& s) { return s.get_box_dimensions(); },
-            "Box dimensions and angles")
-        // 保留原有方法
-        .def("add_atom", &model::Structure::add_atom)
-        .def("add_residue", &model::Structure::add_residue)
-        .def("add_terminal", &model::Structure::add_terminal)
-        .def("add_helix", &model::Structure::add_helix)
-        .def("add_sheet", &model::Structure::add_sheet)
-        .def("add_ssbond", &model::Structure::add_ssbond)
-        .def("set_box_dimensions", &model::Structure::set_box_dimensions)
-        .def("clear", &model::Structure::clear);
-
     // Bind PDBParser to io submodule
     auto pdb_parser = py::class_<io::PDBParser>(io, "PDBParser")
         .def_static("parse_file", &io::PDBParser::parse_file,
--- src/bindings/model_bindings.cpp
+++ src/bindings/model_bindings.cpp
@@ -5,6 +5,7 @@
 #include "model/atom.hpp"
 #include "model/residue.hpp"
 #include "model/topology.hpp"
+#include "model/structure.hpp"
 #include "model/forcefield.hpp"
 
 namespace py = pybind11;
@@ -16,6 +17,56 @@ namespace bindings {
 void init_model(py::module& m) {
     // Create model submodule
     auto model = m.def_submodule("model", "Data model classes");
+    
+    // Bind Structure::SecondaryStructure
+    auto secondary_structure = py::class_<model::Structure::SecondaryStructure>(m, "SecondaryStructure")
+        .def(py::init<>())
+        .def_readwrite("id", &model::Structure::SecondaryStructure::id)
+        .def_readwrite("initResName", &model::Structure::SecondaryStructure::initResName)
+        .def_readwrite("initChainId", &model::Structure::SecondaryStructure::initChainId)
+        .def_readwrite("initSeqNum", &model::Structure::SecondaryStructure::initSeqNum)
+        .def_readwrite("initICode", &model::Structure::SecondaryStructure::initICode)
+        .def_readwrite("endResName", &model::Structure::SecondaryStructure::endResName)
+        .def_readwrite("endChainId", &model::Structure::SecondaryStructure::endChainId)
+        .def_readwrite("endSeqNum", &model::Structure::SecondaryStructure::endSeqNum)
+        .def_readwrite("endICode", &model::Structure::SecondaryStructure::endICode)
+        .def_readwrite("helixClass", &model::Structure::SecondaryStructure::structureClass);
+
+    // Bind Structure::TerminalInfo
+    auto terminal_info = py::class_<model::Structure::TerminalInfo>(m, "TerminalInfo")
+        .def(py::init<>())
+        .def_readwrite("chainId", &model::Structure::TerminalInfo::chainId)
+        .def_readwrite("resSeq", &model::Structure::TerminalInfo::resSeq)
+        .def_readwrite("iCode", &model::Structure::TerminalInfo::iCode)
+        .def_readwrite("resName", &model::Structure::TerminalInfo::resName);
+
+    // Bind Structure to main module
+    auto structure = py::class_<model::Structure>(m, "Structure")
+        .def(py::init<>())
+        // 直接暴露内部成员作为属性
+        .def_property_readonly("atoms", [](const model::Structure& s) { return s.get_atoms(); },
+            "List of atoms in the structure")
+        .def_property_readonly("residues", [](const model::Structure& s) { return s.get_residues(); },
+            "List of residues in the structure")
+        .def_property_readonly("terminals", [](const model::Structure& s) { return s.get_terminals(); },
+            "List of terminal records")
+        .def_property_readonly("helices", [](const model::Structure& s) { return s.get_helices(); },
+            "Map of chain IDs to helix information")
+        .def_property_readonly("sheets", [](const model::Structure& s) { return s.get_sheets(); },
+            "Map of chain IDs to sheet information")
+        .def_property_readonly("ssbonds", [](const model::Structure& s) { return s.get_ssbonds(); },
+            "List of disulfide bonds")
+        .def_property_readonly("box_dimensions", [](const model::Structure& s) { return s.get_box_dimensions(); },
+            "Box dimensions and angles")
+        // 保留原有方法
+        .def("add_atom", &model::Structure::add_atom)
+        .def("add_residue", &model::Structure::add_residue)
+        .def("add_terminal", &model::Structure::add_terminal)
+        .def("add_helix", &model::Structure::add_helix)
+        .def("add_sheet", &model::Structure::add_sheet)
+        .def("add_ssbond", &model::Structure::add_ssbond)
+        .def("set_box_dimensions", &model::Structure::set_box_dimensions)
+        .def("clear", &model::Structure::clear);
     
     // Bind Atom class
     py::class_<model::Atom, std::shared_ptr<model::Atom>>(model, "PDBAtom")
