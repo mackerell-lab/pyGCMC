3bd8b603b60d06fb2d7467ef0c0e1466885cb9c5
Xenopelt <zhaomingtian@gmail.com>
2025-06-23 03:39:41 -0400

    Remove donor and acceptor checks

--- src/model/topology/TopologyBonds.hpp
+++ src/model/topology/TopologyBonds.hpp
@@ -251,27 +251,6 @@ public:
             });
     }
 
-    bool has_donor(int donor_atom) const {
-        return std::any_of(storage_.donors.begin(), storage_.donors.end(),
-            [donor_atom](const TopologyDonor& donor) {
-                return donor.donor_atom == donor_atom;
-            });
-    }
-
-    bool has_donor(int donor_atom, int hydrogen_atom) const {
-        return std::any_of(storage_.donors.begin(), storage_.donors.end(),
-            [donor_atom, hydrogen_atom](const TopologyDonor& donor) {
-                return donor.donor_atom == donor_atom && donor.hydrogen_atom == hydrogen_atom;
-            });
-    }
-
-    bool has_acceptor(int acceptor_atom) const {
-        return std::any_of(storage_.acceptors.begin(), storage_.acceptors.end(),
-            [acceptor_atom](const TopologyAcceptor& acceptor) {
-                return acceptor.acceptor_atom == acceptor_atom;
-            });
-    }
-
     bool has_cmap() const {
         return !storage_.cmaps.empty();
     }
