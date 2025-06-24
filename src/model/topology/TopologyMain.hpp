d9b49fda18115186d9687b1f06b7d285ff12101e
Xenopelt <zhaomingtian@gmail.com>
2025-06-23 21:17:00 -0400

    Implement TopologyStats and remove unused files

--- src/model/topology/TopologyMain.hpp
+++ src/model/topology/TopologyMain.hpp
@@ -58,112 +58,112 @@ public:
     }
 
     // Atom management - delegate to AtomManager
-    inline int add_atom(const std::string& name, const std::string& type, double charge, double mass,
+    int add_atom(const std::string& name, const std::string& type, double charge, double mass,
                 const std::string& residue_name, int residue_number, const std::string& segment_name) {
         return atom_manager_.add_atom(name, type, charge, mass, residue_name, residue_number, segment_name);
     }
 
-    inline int add_residue(const std::string& name, int number, const std::string& segment) {
+    int add_residue(const std::string& name, int number, const std::string& segment) {
         return atom_manager_.add_residue(name, number, segment);
-                }
+    }
 
-    inline int add_segment(const std::string& name) {
+    int add_segment(const std::string& name) {
         return atom_manager_.add_segment(name);
     }
 
     // Bond management - delegate to BondManager
-    inline void add_bond(int atom1, int atom2, double length = 0.0, double force_constant = 0.0, int function_type = 1) {
+    void add_bond(int atom1, int atom2, double length = 0.0, double force_constant = 0.0, int function_type = 1) {
         bond_manager_.add_bond(atom1, atom2, length, force_constant, function_type);
     }
 
-    inline void add_angle(int atom1, int atom2, int atom3, double angle = 0.0, double force_constant = 0.0, int function_type = 1) {
+    void add_angle(int atom1, int atom2, int atom3, double angle = 0.0, double force_constant = 0.0, int function_type = 1) {
         bond_manager_.add_angle(atom1, atom2, atom3, angle, force_constant, function_type);
     }
 
-    inline void add_dihedral(int atom1, int atom2, int atom3, int atom4, int multiplicity = 1,
+    void add_dihedral(int atom1, int atom2, int atom3, int atom4, int multiplicity = 1,
                      double angle = 0.0, double force_constant = 0.0, bool improper = false, int function_type = 1) {
         bond_manager_.add_dihedral(atom1, atom2, atom3, atom4, multiplicity, angle, force_constant, improper, function_type);
     }
 
-    inline void add_improper(int atom1, int atom2, int atom3, int atom4, double angle = 0.0, double force_constant = 0.0) {
+    void add_improper(int atom1, int atom2, int atom3, int atom4, double angle = 0.0, double force_constant = 0.0) {
         bond_manager_.add_improper(atom1, atom2, atom3, atom4, angle, force_constant);
     }
 
     // Hydrogen bond management - delegate to HBondManager
-    inline void add_donor(int donor, int hydrogen) {
+    void add_donor(int donor, int hydrogen) {
         hbond_manager_.add_donor(donor, hydrogen);
     }
 
-    inline void add_acceptor(int acceptor) {
+    void add_acceptor(int acceptor) {
         hbond_manager_.add_acceptor(acceptor);
-        }
+    }
 
     // Special features - delegate to SpecialManager
-    inline void add_nonbonded_exclusion(int atom1, int atom2) {
+    void add_nonbonded_exclusion(int atom1, int atom2) {
         special_manager_.add_nonbonded_exclusion(atom1, atom2);
     }
 
-    inline void add_group(int id, const std::vector<int>& atoms, const std::string& type = "") {
+    void add_group(int id, const std::vector<int>& atoms, const std::string& type = "") {
         special_manager_.add_group(id, atoms, type);
     }
 
-    inline void add_cmap(const std::array<int, 8>& atoms) {
+    void add_cmap(const std::array<int, 8>& atoms) {
         special_manager_.add_cmap(atoms);
     }
 
-    inline void add_cmap(const std::array<int, 5>& atoms, int function_type = 1) {
+    void add_cmap(const std::array<int, 5>& atoms, int function_type = 1) {
         special_manager_.add_cmap(atoms, function_type);
     }
 
     // Getters - delegate to appropriate managers
-    inline const TopologyAtom& get_atom(int index) const { return atom_manager_.get_atom(index); }
-    inline const TopologyResidue& get_residue(int index) const { return atom_manager_.get_residue(index); }
-    inline const TopologySegment& get_segment(int index) const { return atom_manager_.get_segment(index); }
+    const TopologyAtom& get_atom(int index) const { return atom_manager_.get_atom(index); }
+    const TopologyResidue& get_residue(int index) const { return atom_manager_.get_residue(index); }
+    const TopologySegment& get_segment(int index) const { return atom_manager_.get_segment(index); }
 
-    inline const std::vector<TopologyBond>& get_bonds() const { return bond_manager_.get_bonds(); }
-    inline const std::vector<TopologyAngle>& get_angles() const { return bond_manager_.get_angles(); }
-    inline const std::vector<TopologyDihedral>& get_dihedrals() const { return bond_manager_.get_dihedrals(); }
+    const std::vector<TopologyBond>& get_bonds() const { return bond_manager_.get_bonds(); }
+    const std::vector<TopologyAngle>& get_angles() const { return bond_manager_.get_angles(); }
+    const std::vector<TopologyDihedral>& get_dihedrals() const { return bond_manager_.get_dihedrals(); }
 
-    inline const std::vector<TopologyDonor>& get_donors() const { return hbond_manager_.get_donors(); }
-    inline const std::vector<TopologyAcceptor>& get_acceptors() const { return hbond_manager_.get_acceptors(); }
+    const std::vector<TopologyDonor>& get_donors() const { return hbond_manager_.get_donors(); }
+    const std::vector<TopologyAcceptor>& get_acceptors() const { return hbond_manager_.get_acceptors(); }
 
-    inline const std::vector<TopologyGroup>& get_groups() const { return special_manager_.get_groups(); }
-    inline const std::vector<TopologyCmap>& get_cmaps() const { return special_manager_.get_cmaps(); }
-    inline const std::map<int, std::set<int>>& get_exclusions() const { return special_manager_.get_exclusions(); }
+    const std::vector<TopologyGroup>& get_groups() const { return special_manager_.get_groups(); }
+    const std::vector<TopologyCmap>& get_cmaps() const { return special_manager_.get_cmaps(); }
+    const std::map<int, std::set<int>>& get_exclusions() const { return special_manager_.get_exclusions(); }
 
     // Utility functions
-    inline bool has_atom(int index) const { return atom_manager_.has_atom(index); }
-    inline bool has_residue(int index) const { return atom_manager_.has_residue(index); }
-    inline bool has_segment(int index) const { return atom_manager_.has_segment(index); }
+    bool has_atom(int index) const { return atom_manager_.has_atom(index); }
+    bool has_residue(int index) const { return atom_manager_.has_residue(index); }
+    bool has_segment(int index) const { return atom_manager_.has_segment(index); }
 
-    inline int get_num_atoms() const { return atom_manager_.get_num_atoms(); }
-    inline int get_num_residues() const { return atom_manager_.get_num_residues(); }
-    inline int get_num_segments() const { return atom_manager_.get_num_segments(); }
+    int get_num_atoms() const { return atom_manager_.get_num_atoms(); }
+    int get_num_residues() const { return atom_manager_.get_num_residues(); }
+    int get_num_segments() const { return atom_manager_.get_num_segments(); }
 
     // Count methods - delegate to appropriate managers
-    inline size_t get_num_bonds() const { return bond_manager_.get_num_bonds(); }
-    inline size_t get_num_angles() const { return bond_manager_.get_num_angles(); }
-    inline size_t get_num_dihedrals() const { return bond_manager_.get_num_dihedrals(); }
-    inline size_t get_num_impropers() const { return bond_manager_.get_num_impropers(); }
+    size_t get_num_bonds() const { return bond_manager_.get_num_bonds(); }
+    size_t get_num_angles() const { return bond_manager_.get_num_angles(); }
+    size_t get_num_dihedrals() const { return bond_manager_.get_num_dihedrals(); }
+    size_t get_num_impropers() const { return bond_manager_.get_num_impropers(); }
 
-    inline size_t get_num_donors() const { return hbond_manager_.get_num_donors(); }
-    inline size_t get_num_acceptors() const { return hbond_manager_.get_num_acceptors(); }
+    size_t get_num_donors() const { return hbond_manager_.get_num_donors(); }
+    size_t get_num_acceptors() const { return hbond_manager_.get_num_acceptors(); }
 
-    inline size_t get_num_cmaps() const { return special_manager_.get_num_cmaps(); }
-    inline size_t get_num_groups() const { return special_manager_.get_num_groups(); }
+    size_t get_num_cmaps() const { return special_manager_.get_num_cmaps(); }
+    size_t get_num_groups() const { return special_manager_.get_num_groups(); }
 
     // Check methods - delegate to appropriate managers
-    inline bool has_bond(int atom1, int atom2) const { return bond_manager_.has_bond(atom1, atom2); }
-    inline bool has_angle(int atom1, int atom2, int atom3) const { return bond_manager_.has_angle(atom1, atom2, atom3); }
-    inline bool has_dihedral(int atom1, int atom2, int atom3, int atom4) const { return bond_manager_.has_dihedral(atom1, atom2, atom3, atom4); }
-    inline bool has_improper(int atom1, int atom2, int atom3, int atom4) const { return bond_manager_.has_improper(atom1, atom2, atom3, atom4); }
+    bool has_bond(int atom1, int atom2) const { return bond_manager_.has_bond(atom1, atom2); }
+    bool has_angle(int atom1, int atom2, int atom3) const { return bond_manager_.has_angle(atom1, atom2, atom3); }
+    bool has_dihedral(int atom1, int atom2, int atom3, int atom4) const { return bond_manager_.has_dihedral(atom1, atom2, atom3, atom4); }
+    bool has_improper(int atom1, int atom2, int atom3, int atom4) const { return bond_manager_.has_improper(atom1, atom2, atom3, atom4); }
 
-    inline bool has_donor(int donor_atom) const { return hbond_manager_.has_donor(donor_atom); }
-    inline bool has_donor(int donor_atom, int hydrogen_atom) const { return hbond_manager_.has_donor(donor_atom, hydrogen_atom); }
-    inline bool has_acceptor(int acceptor_atom) const { return hbond_manager_.has_acceptor(acceptor_atom); }
+    bool has_donor(int donor_atom) const { return hbond_manager_.has_donor(donor_atom); }
+    bool has_donor(int donor_atom, int hydrogen_atom) const { return hbond_manager_.has_donor(donor_atom, hydrogen_atom); }
+    bool has_acceptor(int acceptor_atom) const { return hbond_manager_.has_acceptor(acceptor_atom); }
     
-    inline bool has_cmap() const { return special_manager_.has_cmap(); }
-    inline bool has_group(int group_id) const { return special_manager_.has_group(group_id); }
+    bool has_cmap() const { return special_manager_.has_cmap(); }
+    bool has_group(int group_id) const { return special_manager_.has_group(group_id); }
 
     // Find elements - delegate to AtomManager
     std::optional<int> find_atom(const std::string& residue_name, int residue_number, const std::string& atom_name) const {
