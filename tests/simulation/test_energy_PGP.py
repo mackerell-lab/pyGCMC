645acba208d7b0ebfc4f670fcea6c998c852b4d5
Xenopelt <zhaomingtian@gmail.com>
2025-07-14 23:01:08 -0400

    Comment out PGP test imports

--- tests/simulation/test_energy_PGP.py
+++ tests/simulation/test_energy_PGP.py
@@ -202,16 +202,16 @@ from energyPGP.pgp_lj_diagnosis import (
     test_pgp_mixed_system,
     test_pgp_lj_distance_scan
 )
-from energyPGP.pgp_pme_debug_electrostatic import (
-    test_simple_two_particle_system,
-    test_pgp_self_consistency
-)
+# from energyPGP.pgp_pme_debug_electrostatic import (
+#     test_simple_two_particle_system,
+#     test_pgp_self_consistency
+# )
 from energyPGP.debug_movement_residues import (
     test_movement_residues
 )
-from energyPGP.debug_vdw_movement import (
-    test_vdw_movement_debug
-)
+# from energyPGP.debug_vdw_movement import (
+#     test_vdw_movement_debug
+# )
 
 # Support direct execution for testing
 if __name__ == "__main__":
